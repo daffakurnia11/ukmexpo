afba325c4f6bb3cb66817dcc040fad615ada0a73
daffakurnia11 <daffakurniaf11@gmail.com>
2021-08-17 17:49:42 +0700

    Add Sponsorship on Footer

--- application/views/main/templates/footer.php
+++ application/views/main/templates/footer.php
@@ -6,7 +6,7 @@
         <img src="<?= base_url('assets/img/'); ?>logo/logo_white.png" class="contact-brand" alt="">
       </div>
       <div class="col-lg-7 order-1 order-lg-2">
-        <div class="footer-sponsor text-center">
+        <div class="footer-support text-center">
           <img class="" src="<?= base_url('assets/img/footer/'); ?>ADVHUM_PUTIH.png" alt="">
           <img class="" src="<?= base_url('assets/img/footer/'); ?>DITMAWA.png" alt="">
           <img class="" src="<?= base_url('assets/img/footer/'); ?>DIES_61.png" alt="">
@@ -15,27 +15,33 @@
           <img class="" src="<?= base_url('assets/img/footer/'); ?>UKM_EXPO.png" alt="">
           <img class="" src="<?= base_url('assets/img/footer/'); ?>KAMPUS_MERDEKA.png" alt="">
         </div>
-        <div class="footer-contact text-center">
-          <h3 class="contact-title">Official Account</h3>
-          <div class="contact-content mx-auto">
-            <a href="https://www.instagram.com/ukmexpo_its/" class="contact-link" target="_blank">
-              <div class="contact-logo me-2">
-                <i class="fab fa-lg fa-instagram"></i>
-              </div>
-              <span class="contact-desc">@ukmexpo_its</span>
-            </a>
-            <a href="https://timeline.line.me/user/_dSmeMWRgIHnuqwa5Fv4Gbs5lRqAhDAlffx8vHjw" class="contact-link" target="_blank">
-              <div class="contact-logo me-2">
-                <i class="fab fa-lg fa-line"></i>
-              </div>
-              <span class="contact-desc">@yay7605b</span>
-            </a>
-            <a href="https://www.youtube.com/channel/UCRcX486rcfh6PrhcH80wVWw?sub_confirmation=1" class="contact-link" target="_blank">
-              <div class="contact-logo me-2">
-                <i class="fab fa-lg fa-youtube"></i>
-              </div>
-              <span class="contact-desc">UKM EXPO ITS</span>
-            </a>
+        <div class="row mb-4">
+          <div class="col-md-5 footer-contact text-center">
+            <h3 class="contact-title">Official Account</h3>
+            <div class="contact-content mx-auto">
+              <a href="https://www.instagram.com/ukmexpo_its/" class="contact-link" target="_blank">
+                <div class="contact-logo me-2">
+                  <i class="fab fa-lg fa-instagram"></i>
+                </div>
+                <span class="contact-desc">@ukmexpo_its</span>
+              </a>
+              <a href="https://timeline.line.me/user/_dSmeMWRgIHnuqwa5Fv4Gbs5lRqAhDAlffx8vHjw" class="contact-link" target="_blank">
+                <div class="contact-logo me-2">
+                  <i class="fab fa-lg fa-line"></i>
+                </div>
+                <span class="contact-desc">@yay7605b</span>
+              </a>
+              <a href="https://www.youtube.com/channel/UCRcX486rcfh6PrhcH80wVWw?sub_confirmation=1" class="contact-link" target="_blank">
+                <div class="contact-logo me-2">
+                  <i class="fab fa-lg fa-youtube"></i>
+                </div>
+                <span class="contact-desc">UKM EXPO ITS</span>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-7 footer-contact text-center">
+            <h3 class="contact-title">Sponsor</h3>
+            <img src="<?= base_url('assets/img/logo/sponsor.png'); ?>" class="footer-sponsor" alt="">
           </div>
         </div>
       </div>
--- application/views/virtualexpo/index.php
+++ application/views/virtualexpo/index.php
@@ -3,37 +3,32 @@
 
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=7">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>UKM EXPO 2021 Virtual Booth</title>
-  <link rel="shortcut icon" href="TemplateData/favicon.ico">
-  <link rel="stylesheet" href="TemplateData/style.css">
+  <link rel="shortcut icon" href="<?= base_url('assets/img/'); ?>logo/logo.png" type="image/x-icon">
+  <link rel="stylesheet" href="<?= base_url('assets/virtualbooth/'); ?>TemplateData/style.css">
   <script src="<?= base_url('assets/virtualbooth/'); ?>TemplateData/UnityProgress.js"></script>
   <script src="<?= base_url('assets/virtualbooth/'); ?>Build/UnityLoader.js"></script>
   <script>
-    var unityInstance = UnityLoader.instantiate("unityContainer", "<?= base_url('assets/virtualbooth/'); ?>Build/vtour 7.json", {
+    var unityInstance = UnityLoader.instantiate("unityContainer", "<?= base_url('assets/virtualbooth/'); ?>Build/Vtour 8.json", {
       onProgress: UnityProgress
     });
   </script>
-  <style>
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-
-    body {
-      overflow: hidden;
-    }
-  </style>
 </head>
 
 <body>
   <div class="webgl-content">
-    <div id="unityContainer" style="width: 100vw; height: 100vh"></div>
+    <div id="unityContainer"></div>
     <div class="footer">
       <div class="webgl-logo"></div>
       <div class="fullscreen" onclick="unityInstance.SetFullscreen(1)"></div>
-      <!-- <div class="title">Vtour UKM</div> -->
+      <div class="title">Virtual Tour UKM Expo 2021</div>
+    </div>
+    <div class="state-bg">
+      <img src="<?= base_url('assets/img/svg/state-flower.svg'); ?>" alt="">
+      <img src="<?= base_url('assets/img/svg/state-flower.svg'); ?>" alt="">
     </div>
   </div>
 </body>
